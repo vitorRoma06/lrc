1c467eb3d179d2236265dc5345c8c2beaa0b8d70
Vitor de Roma <105527692+vitorRoma06@users.noreply.github.com>
2023-05-08 21:10:42 -0300

    Update topo_sw2_host4.py

--- topo_sw2_host4.py
+++ topo_sw2_host4.py
@@ -1,34 +1,32 @@
-"""Custom topology example
-
-Two directly connected switches plus a host for each switch:
-
-   host --- switch --- switch --- host
-
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
-"""
-
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
 
-class MyTopo( Topo ):
-    "Simple topology example."
+class MyTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
 
-    def __init__( self ):
-        "Create custom topo."
+        # Add hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
 
-        # Initialize topology
-        Topo.__init__( self )
-
-        # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        # Add switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
 
         # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+        self.addLink(h1, s1, bw=10, delay='1ms', loss=0)
+        self.addLink(h3, s1, bw=10, delay='1ms', loss=0)
+        self.addLink(h2, s2, bw=10, delay='1ms', loss=0)
+        self.addLink(h4, s2, bw=10, delay='1ms', loss=0)
+        self.addLink(s1, s2, bw=10, delay='1ms', loss=0)
 
-
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+if __name__ == '__main__':
+    topo = MyTopo()
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    net.start()
+    net.pingAll()
+    net.stop()
